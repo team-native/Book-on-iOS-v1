d97958ea6bad10e64c5bc318c51d637766263590
김준표juunpy0 <junpyokim0106@gmail.com>
2026-05-26 18:16:28 +0900

    Test

---
+++ Book-on-iOS-v1/Book_on_iOS_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Book_on_iOS_v1App.swift
+//  Book-on-iOS-v1
+//
+//  Created by 김준표 on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Book_on_iOS_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
