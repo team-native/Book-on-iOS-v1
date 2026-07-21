127551ae1e03c42291c15a44d9694d472e9337a7
Book-on <s40409841@gmail.com>
2026-07-21 17:28:09 +0900

    feat : [#50] 공통 API 응답 및 네트워크 클라이언트 구현

---
+++ Projects/Service/Sources/Network/APIResponse.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public struct APIResponse<ResponseData: Decodable>: Decodable {
+    public let errorCode: Int
+    public let message: String
+    public let data: ResponseData
+
+    public init(errorCode: Int, message: String, data: ResponseData) {
+        self.errorCode = errorCode
+        self.message = message
+        self.data = data
+    }
+}
+
+struct APIErrorResponse: Decodable {
+    let errorCode: Int
+    let message: String
+}
